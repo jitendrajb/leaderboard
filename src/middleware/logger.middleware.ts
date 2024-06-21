5b6780cd35796cb5f0f9486cdeac4bef48f1e117
Jitendra.jini <62359040+jitendrajb@users.noreply.github.com>
2024-06-21 13:38:41 +0530

    Update logger.middleware.ts

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -11,10 +11,27 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logStream = fs.createWriteStream(path.join(__dirname, '../../access.log'), { flags: 'a' });
 
   use(req: Request, res: Response, next: NextFunction) {
-    morgan.token('client-ip', function(req) {
-      return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    });
+    morgan.token('client-ip', function(req:Request) {
+      const xForwardedFor = req.headers['x-forwarded-for'];
+      let clientIp: string;
 
+      if (Array.isArray(xForwardedFor)) {
+        clientIp = xForwardedFor.join(', ');
+      } else if (typeof xForwardedFor === 'string') {
+        clientIp = xForwardedFor;
+      } else {
+        clientIp = req.socket.remoteAddress || '';
+      }
+
+      // Convert IPv6 localhost to IPv4 localhost
+      if (clientIp === '::1' || clientIp === '::') {
+        clientIp = '127.0.0.1';
+      }
+
+      return clientIp;
+
+    });
+    
     morgan(':client-ip :method :url :status', { stream: this.logStream })(req, res, next);
   }
 }
